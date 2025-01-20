1670d07b37321183b94be028cf8036fcfa1fe74b
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-01-20 14:45:06 +0100

    houji: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: xiaomi Model: Xiaomi 14 Script version: 3.0.0

---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for Xiaomi Xiaomi 14 (houji)
+
+```
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_houji-user
+add_lunch_combo omni_houji-userdebug
+add_lunch_combo omni_houji-eng
